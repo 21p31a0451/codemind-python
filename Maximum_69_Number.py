8a8dff8bd735e01aedd78b1c0be7f104f490c050
Naveen kumar Saginela <99870668+21p31a0451@users.noreply.github.com>
2023-05-30 13:53:09 +0530

    n=str(input()) a=n.replace('6','9',1) print(a)

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,21 +1,3 @@
-n=int(input())
-tmp=str(n)
-a=''
-for i in tmp:
-    a=a+i
-z=tmp.find('6')
-if z==-1:
-    print(n)
-elif z==0:
-    n+=3000
-    print(n)
-elif z==1:
-    n+=300
-    print(n)
-elif z==2:
-    n+=30
-    print(n)
-elif z==3:
-    n+=3
-    print(n)
-    
+n=str(input())
+a=n.replace('6','9',1)
+print(a)
